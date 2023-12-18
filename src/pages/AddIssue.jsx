94847cfcdc49c8eed42cb12e7f4380cc3ae6b1e1
Neamul01 <neamulhaque2002@gmail.com>
2023-12-18 20:56:00 +0600

    issue add done

--- src/pages/AddIssue.jsx
+++ src/pages/AddIssue.jsx
@@ -1,3 +1,44 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
 export default function AddIssue() {
-  return <h2>Add Issue</h2>;
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const addIssue = useMutation({
+    mutationFn: (issueBody) =>
+      fetch("/api/issues", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(issueBody),
+      }).then((res) => res.json()),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["issues"], exact: true });
+      queryClient.setQueryData(["issues", data.number.toString()], data);
+      navigate(`issue/${data.number}`);
+    },
+  });
+  return (
+    <div className="add-issue">
+      <h2>Add Issue</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (addIssue.isPending) return;
+          addIssue.mutate({
+            comment: e.target.comment.value,
+            title: e.target.title.value,
+          });
+        }}
+      >
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" placeholder="Title" name="title" />
+        <label htmlFor="comment"> Comment</label>
+        <textarea placeholder="Comment" id="comment" name="comment" />
+        <button type="submit" disabled={addIssue.isPending}>
+          {addIssue.isPending ? "Adding Issue..." : "Add Issue"}
+        </button>
+      </form>
+    </div>
+  );
 }
