9120c6818092900121be54dd861bdb3a95a308e0
Neamul01 <neamulhaque2002@gmail.com>
2023-12-19 12:56:59 +0600

    pagination added: error in keepPreviousData

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -3,27 +3,26 @@ import { IssueItem } from "./IssueItem";
 import fetchWithError from "../helpers/fetchWithError";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import Loader from "./Loader";
-// import { useQuery } from "react-query";
 
-export default function IssuesList({ labels, status }) {
+export default function IssuesList({ labels, status, pageNum, setPageNum }) {
   const queryClient = useQueryClient();
 
+  const fetchIssues = async ({ queryKey }) => {
+    const [_key, { labels, status, pageNum }] = queryKey;
+    const statusString = status ? `&status=${status}` : "";
+    const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
+    const paginationStr = `&page=${pageNum}`;
+
+    return fetchWithError(
+      `/api/issues?${labelsString}${statusString}${paginationStr}`
+    );
+  };
+
+  // FIXME: previous data are not kept
   const issuesQuery = useQuery({
-    queryKey: ["issues", { labels, status }],
-    queryFn: async ({ signal }) => {
-      const statusString = status ? `&status=${status}` : "";
-      const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
-      const results = await fetchWithError(
-        `/api/issues?${labelsString}${statusString}`,
-        {
-          signal,
-        }
-      );
-      results.forEach((issue) => {
-        queryClient.setQueryData(["issues", issue.number.toString()], issue);
-      });
-      return results;
-    },
+    queryKey: ["issues", { labels, status, pageNum }],
+    queryFn: fetchIssues,
+    keepPreviousData: true,
   });
   const [searchValue, setSearchValue] = useState("");
 
@@ -36,8 +35,8 @@ export default function IssuesList({ labels, status }) {
 
     enabled: searchValue.length > 0,
   });
-  console.log(searchQuery.data);
-  console.log("issues query", issuesQuery.data);
+  // console.log(searchQuery.data);
+  // console.log("issues query", issuesQuery.data);
 
   return (
     <div>
@@ -60,27 +59,47 @@ export default function IssuesList({ labels, status }) {
           }}
         />
       </form>
-      <h2>Issues List {issuesQuery.isFetching ? <Loader /> : null}</h2>
+      <h2>
+        Issues List{" "}
+        {issuesQuery.isFetching && !issuesQuery.isLoading ? <Loader /> : null}
+      </h2>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
       ) : issuesQuery.isError ? (
         <p>{issuesQuery.error.message}</p>
       ) : searchValue.length === 0 || !searchQuery.data ? (
-        <ul className="issues-list">
-          {issuesQuery.data.map((issue) => (
-            <IssueItem
-              key={issue.id}
-              title={issue.title}
-              number={issue.number}
-              assignee={issue.assignee}
-              commentCount={issue.comments.length}
-              createdBy={issue.createdBy}
-              createdDate={issue.createdDate}
-              labels={issue.labels}
-              status={issue.status}
-            />
-          ))}
-        </ul>
+        <>
+          <ul className="issues-list">
+            {issuesQuery.data?.map((issue) => (
+              <IssueItem
+                key={issue.id}
+                title={issue.title}
+                number={issue.number}
+                assignee={issue.assignee}
+                commentCount={issue.comments.length}
+                createdBy={issue.createdBy}
+                createdDate={issue.createdDate}
+                labels={issue.labels}
+                status={issue.status}
+              />
+            ))}
+          </ul>
+          <div className="pagination">
+            <button
+              onClick={() => setPageNum((old) => Math.max(old - 1, 1))}
+              disabled={pageNum === 1}
+            >
+              Previous
+            </button>
+            <p>Page {pageNum}</p>
+            <button
+              onClick={() => setPageNum((old) => old + 1)}
+              disabled={issuesQuery.isFetching || issuesQuery.isLoading}
+            >
+              Next
+            </button>
+          </div>
+        </>
       ) : (
         <>
           <h2>Search Results</h2>
